54c22a9bc51f8b9f673248126f72a73c48e83b95
Emiliano Mariño <emiliano.marino@valtech.com>
2019-10-04 11:04:03 -0400

    secrets

---
+++ .secrets.sh
@@ -0,0 +1,13 @@
+# AWS
+export AWS_ACCESS_KEY_ID=
+echo "AWS_ACCESS_KEY_ID environment variable set"
+export AWS_SECRET_ACCESS_KEY=
+echo "AWS_SECRET_ACCESS_KEY environment variable set"
+
+# Terraform Cloud
+export TF_TOKEN=QKNLXMLaIUJSlg.atlasv1.3QdPIjQnNqZuppy6pGKyMGmrVzi39RqQtdGwGSrNenfV23HY64jVOybRrFzuTQnZz7I
+echo "TF_TOKEN environment variable set"
+export TF_VAR_private_key=$(/bin/cat .ssh/id_rsa | base64)
+echo "TF_VAR_private_key environment variable set"
+export TF_VAR_public_key=$(/bin/cat .ssh/id_rsa.pub | base64)
+echo "TF_VAR_public_key environment variable set"
