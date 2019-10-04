698181bfc27a8a02a475a9d5049fd17d788f13f4
Emiliano Mariño <emiliano.marino@valtech.com>
2019-10-04 12:05:10 -0400

    worspace id

---
+++ .env.sh
@@ -0,0 +1,19 @@
+# Terraform Workspace Variables
+export TF_VAR_ami=ami-07d0cf3af28718ef8
+echo "TF_VAR_ami=$TF_VAR_ami"
+export TF_VAR_identity=
+echo "TF_VAR_identity=$TF_VAR_identity"
+export TF_VAR_subnet_id=subnet-0ee95f34e3e3c4052
+echo "TF_VAR_subnet_id=$TF_VAR_subnet_id"
+export TF_VAR_vpc_security_group_id=sg-0c280b01eeb30aa2d
+echo "TF_VAR_vpc_security_group_id=$TF_VAR_vpc_security_group_id"
+
+# Terraform
+export TF_CLI_CONFIG_FILE=.terraformrc
+echo "TF_CLI_CONFIG_FILE=$TF_CLI_CONFIG_FILE"
+export TF_ORGANIZATION=zx-ventures
+echo "TF_ORGANIZATION=$TF_ORGANIZATION"
+export TF_WORKSPACE=demo-terraform-101-emiliano
+echo "TF_WORKSPACE=$TF_WORKSPACE"
+export TF_WORKSPACE_ID=ws-VopQCJcrf5eVSW5E
+echo "TF_WORKSPACE_ID=$TF_WORKSPACE_ID"
